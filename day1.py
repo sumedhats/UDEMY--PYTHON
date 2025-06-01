3b49b66ba1c6b1e55d59049949ab52dd73e4b873
sumedha <sumedhts2020@gmail.com>
2025-06-01 13:51:15 +0530

    add day 1 file

---
+++ day1.py
@@ -0,0 +1,57 @@
+ #practice 1
+ # Write a program that uses print statements to print the following recipe
+ #into the Output console. The text to print is already there, you just need to make it 
+ #into code. Your code should print all five lines exactly the same as the example output below.
+ # Make sure that you don't change any of these existing text as everything, punctuation and casing 
+ # all need to match!
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+#practice 2 
+#Look at the code in the code editor. There are 
+#errors on all 5 lines of code. Fix the code so that it runs without errors.
+#Try to run the code and debug each line using the error messages and feedback.
+# Fix the code below 👇
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+#We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
+glass1 = "milk"
+glass2 = "juice"
+# solution
+glass1 = "juice"
+glass2 = "milk"print("Welcome to the Band Name Generator")
+a = input("what is the name of the city you grew up in? \n")
+b = input("what is the name of your pet? \n")
+print("Your band name could be " + a + " " + b)
+ 
+
+ # code 1
+print ("hello world!")
+ #code 2
+print ("hello world!\nhello world!")
+ #code 3
+print ("hello" + "world")
+ #code 4 
+print ("hello" + " " + "world")
+# #code 5
+print("hello"+ input("what is your name"))
+# # code 6
+name = input ("what is your name ")
+print(name)
+# # CODE 7
+name = "jack"
+print(name)
+# # try 1
+print("hello"  + input("what is your name") + "!")
+# try 2
+name = input ("what is your name ")
+print(len(name))
+#try 3
+username = input("what is your name?")
+length = len(username)
+print(length )
