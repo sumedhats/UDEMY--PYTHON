052ae0ad6028d1395b725e05f6e99bd054d74a3a
sumedhats <sumedhats2020@gmail.com>
2025-06-01 13:40:22 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+ # code 1
+print ("hello world!")
+ #code 2
+print ("hello world!\nhello world!")
+ #code 3
+print ("hello" + "world")
+ #code 4 
+print ("hello" + " " + "world")
+# #code 5
+print("hello"+ input("what is your name"))
+# # code 6
+name = input ("what is your name ")
+print(name)
+# # CODE 7
+name = "jack"
+print(name)
---
+++ try1.py
@@ -0,0 +1,9 @@
+# # try 1
+print("hello"  + input("what is your name") + "!")
+# try 2
+name = input ("what is your name ")
+print(len(name))
+#try 3
+username = input("what is your name?")
+length = len(username)
+print(length )
