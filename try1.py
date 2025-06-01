99a081e91948780859f1e1b00d93d97fac34178a
sumedhats <sumedhats2020@gmail.com>
2025-06-01 13:56:17 +0530

    Delete try1.py

--- try1.py
+++
@@ -1,9 +0,0 @@
-# # try 1
-print("hello"  + input("what is your name") + "!")
-# try 2
-name = input ("what is your name ")
-print(len(name))
-#try 3
-username = input("what is your name?")
-length = len(username)
-print(length )
