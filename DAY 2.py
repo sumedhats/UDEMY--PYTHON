9c37aad6706dfe5d0c65d3eedb0571d2e6c9e2e6
sumedhats <sumedhats2020@gmail.com>
2025-06-02 10:19:45 +0530

    Add files via upload

---
+++ DAY 2.py
@@ -0,0 +1,59 @@
+# code 1
+print("hello"[0])
+#try 
+print("hello"[4])
+#code 2
+print("hello"[-3])
+#code 3
+print("123" + "456")
+#code 4
+print(123 + 456)
+# CODE 5
+print(123_456_789)
+# CODE 6
+print(1.345)
+# CODE 7
+print(True)
+print(False)
+# code 8
+len("345")
+#code 9
+print(type("qq"))
+#try 2
+print(type(123))
+print(type(1.34))
+print(type(False))
+print(type("qq"))
+#code 10
+print(int("123")+ int("456"))
+#try3
+name_of_the_user = input("name ")
+no_of_letters = len(name_of_the_user) 
+print(" name_of_the_user " + str(no_of_letters)) 
+#code 11
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2**3)
+print(2//3)
+print(3*3+ 3/3-3) 
+#task 1 BMI Calculator
+# The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. This is the formula used to calculate it:
+# bmi is equal to the person's weight divided by the person's height squared.
+# Convert this sentence into code on line 6.
+height = 1.65 
+weight = 84 
+bmi = weight/(height**2)
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+# code 12
+score = 0
+score += 1
+print(int(score)) 
+# code 13
+score = 1
+height = 1.4
+is_winning = True
+print(f"score: {score}, height: {height}, is_winning: {is_winning}") 
