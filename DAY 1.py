19bcda8c878b64853663ee8395923a2511ed0369
sumedhats <sumedhats2020@gmail.com>
2025-06-03 08:14:52 +0530

    Update DAY 1.py

--- DAY 1.py
+++ DAY 1.py
@@ -1,4 +1,4 @@
- #practice 1
+ #PRACTICE  1
  # Write a program that uses print statements to print the following recipe
  #into the Output console. The text to print is already there, you just need to make it 
  #into code. Your code should print all five lines exactly the same as the example output below.
