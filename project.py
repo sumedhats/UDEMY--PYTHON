8b14195a748af744996650b64fc4942287e1e2cb
sumedhats <sumedhats2020@gmail.com>
2025-06-04 12:59:31 +0530

    Update project.py

--- project.py
+++ project.py
@@ -62,6 +62,28 @@ elif (choose == "rock" and computer_choices == "scissor") or \
     print("You win!")
 else:
     print("You lose!")
+ #PROJECT 5import random
+
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()"
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_numbers = int(input("How many numbers would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like in your password?\n"))
+
+password = []
+
+for _ in range(nr_letters):
+    password.append(random.choice(letters))
+for _ in range(nr_numbers):
+    password.append(random.choice(numbers))
+for _ in range(nr_symbols):
+    password.append(random.choice(symbols))
+
+random.shuffle(password)
+final_password = ''.join(password)
+print(f"Your password is: {final_password}")
      
 
 
