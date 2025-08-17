34f19e6c18fb0c1bdc765ea726bbd0ff712ba946
sumedhats <sumedhats2020@gmail.com>
2025-08-17 16:35:57 +0530

    Add files via upload

---
+++ daily_series/DAY19.py
@@ -0,0 +1,7 @@
+import turtle as Turtle
+class race:
+   turtle = Turtle.Turtle()
+   ram = Turtle.Turtle()
+   ravi = Turtle.Turtle()
+   # turtle is an object and ram and ravi areobject instance of turtle classcwd
+   
