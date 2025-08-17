28ec666aaf0fd9042ff42ceb422073f70f0fc694
sumedhats <sumedhats2020@gmail.com>
2025-08-17 15:37:24 +0530

    Added Day 19 Python practice file

---
+++ daily_series/DAY 19.py
@@ -0,0 +1,6 @@
+import turtle as Turtle
+class race:
+   turtle = Turtle.Turtle()
+   ram = Turtle.Turtle()
+   ravi = Turtle.Turtle()
+   # turtle is an object and ram and ravi areobject instance of turtle class
