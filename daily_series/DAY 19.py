c5df6015fb6fe128d95e31c5b75e8ec4340f16c7
sumedhats <sumedhats2020@gmail.com>
2025-08-17 15:44:21 +0530

    Clean up duplicate Day 19 file naming

--- daily_series/DAY 19.py
+++
@@ -1,6 +0,0 @@
-import turtle as Turtle
-class race:
-   turtle = Turtle.Turtle()
-   ram = Turtle.Turtle()
-   ravi = Turtle.Turtle()
-   # turtle is an object and ram and ravi areobject instance of turtle class
